fa0ae7866dcf858b80786ff166fc446b7ffe68e4
kangdaehan <kadaha112@gmail.com>
2024-03-27 11:05:27 +0900

    init commit

--- app/src/main/java/activities/LoginActivity.kt
+++ app/src/main/java/activities/LoginActivity.kt
@@ -3,53 +3,75 @@ package activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Toast
-import com.daehankang.comeout.R
 import com.daehankang.comeout.databinding.ActivityLoginBinding
-import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
-    private val binding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.signUp.setOnClickListener {startActivity(Intent(this, activities.SignUpActivity::class.java))
-        finish()}
+        auth = Firebase.auth
 
-        var id : TextInputLayout = binding.inputLayoutEmail
-        var pw : TextInputLayout = binding.inputLayoutPassword
+        binding.signUp.setOnClickListener { startActivity(Intent(this, SignUpActivity::class.java)) }
+        binding.browse.setOnClickListener { startActivity(Intent(this,MainActivity::class.java)) }
+        binding.signUpLoginBtn.setOnClickListener { clickBtnLogin() }
+        binding.clickGoogle.setOnClickListener { clickGoogle() }
+        binding.clickKakao.setOnClickListener { clickKakao() }
+        binding.clickNaver.setOnClickListener { clickNaver() }
 
-        val login = binding.signUpLoginBtn
 
-        auth= Firebase.auth
-
-        login.setOnClickListener {
-            login(id.editText.toString(), pw.editText.toString())
-        }
     }
-    private fun login(id: String, pw: String) {
 
-        if (id.isNullOrEmpty() || pw.isNullOrEmpty()) {
-            Toast.makeText(this, "빈 값을 입력하셨습니다.", Toast.LENGTH_SHORT).show()
-        } else {
-            auth.signInWithEmailAndPassword(id, pw)
-                .addOnCompleteListener { task ->
+    private fun clickBtnLogin(){
+
+            val email = binding.editTextEmail.text.toString().trim()
+            val password = binding.editTextPassword.text.toString().trim()
+
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(applicationContext, "이메일을 입력하세요.", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(applicationContext, "비밀번호를 입력하세요.", Toast.LENGTH_SHORT).show()
+                return
+            }
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
-                        Toast.makeText(this, "로그인에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        val errorMessage = when(task.exception){
+                            is FirebaseAuthInvalidUserException -> "가입되지 않은 이메일입니다."
+                            is FirebaseAuthInvalidCredentialsException -> "잘못된 비밀번호 입니다."
+                            else -> "로그인 실패 : ${task.exception?.message}"
 
-                        val intent = Intent(this, MainActivity::class.java)
-                        startActivity(intent)
                     }
-                    else {
-                        Toast.makeText(this, "아이디와 비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
                     }
                 }
         }
+    private fun clickGoogle(){
+
     }
+    private fun clickKakao(){
+
+    }
+    private fun clickNaver(){
+
+    }
+
 }
--- app/src/main/java/activities/SignUpActivity.kt
+++ app/src/main/java/activities/SignUpActivity.kt
@@ -3,53 +3,79 @@ package activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.daehankang.comeout.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var auth : FirebaseAuth
-    private val binding by lazy { ActivitySignUpBinding.inflate(layoutInflater) }
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestoreDB: FirebaseFirestore
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        var email= binding.signUpEmail
-        var pw= binding.signUpPassword
+        auth = FirebaseAuth.getInstance()
+        firestoreDB = FirebaseFirestore.getInstance()
 
-        binding.btnSignup.setOnClickListener {
-            var email = email.text.toString()
-            var pw = pw.text.toString()
+        binding.signUpButton.setOnClickListener {
+            val email = binding.signEmail.text.toString().trim()
+            val password = binding.signPassword.text.toString().trim()
+            val confirmPassword = binding.signConfirmPassword.text.toString().trim()
 
-            auth = Firebase.auth
-
-            init(email, pw)
-        }
-
-    }
-    private fun init(email: String, pw: String){
-
-        if(email.isNullOrEmpty() || pw.isNullOrEmpty()) {
-            Toast.makeText(this, "이메일 혹은 비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
-        }
-        else {
-            auth.createUserWithEmailAndPassword(email,pw).addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "회원가입 완료", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this, LoginActivity::class.java)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(this, "이메일 형식인지 확인 또는 비밀번호 6자리이상 입력해주세요!", Toast.LENGTH_SHORT).show()
-                }
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(applicationContext, "이메일을 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
             }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(applicationContext, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(confirmPassword)) {
+                Toast.makeText(applicationContext, "비밀번호를 다시 입력해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (password != confirmPassword) {
+                Toast.makeText(applicationContext, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        val user = auth.currentUser
+
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+
+                        if (task.exception is FirebaseAuthUserCollisionException) {
+                            Toast.makeText(
+                                baseContext, "이미 등록된 이메일입니다.",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            Toast.makeText(
+                                baseContext, "회원가입 실패: ${task.exception?.message}",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                }
         }
     }
 }
+
